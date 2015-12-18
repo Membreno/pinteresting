308b50005543f1f9813868a4d4790894230087fe
Shadow W07F <membrep@gmail.com>
2015-12-17 17:05:22 -0800

    fixed scaffold issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217202729) do
+ActiveRecord::Schema.define(version: 20151218000314) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
